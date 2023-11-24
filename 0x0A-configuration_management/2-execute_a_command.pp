65f5370532fa6fa755707e5bb55aa660daa1489f
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2023-11-24 00:34:29 -0800

    create a manifest that kills a process named killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+# create a manifest that kills a process named killmenow
+
+exec { 'kill_killmenow_process':
+  command     => 'pkill killmenow',
+  path        => '/bin:/usr/bin',
+  onlyif      => 'pgrep killmenow',
+  refreshonly => true,
+}
+
