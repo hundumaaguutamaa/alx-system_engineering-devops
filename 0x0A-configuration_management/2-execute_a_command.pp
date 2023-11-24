864ef44dec209699a6373e339d1f15bfcc5f50b5
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-11-24 13:46:12 +0300

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # create a manifest that kills a process named killmenow
-exec { 'kill_killmenow_process':
-  command     => 'pkill killmenow',
-  path        => '/bin:/usr/bin',
+exec { 'kill':
+   command => 'pkill -f killmenow
+   path    => ['/usr/bin', '/usr/sbin']
 }
