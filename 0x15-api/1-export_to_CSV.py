80d48c988a53a3e1943d9ba45793a3f691b2556b
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-01-03 05:00:48 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" using this REST API, export data in the CSV format"""
+"""Using this REST API, export data in the CSV format"""
+
+import csv
+import requests
+import sys
 
 if __name__ == "__main__":
+    # Request user information
+    user_id = sys.argv[1]
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
 
-    import csv
-    import requests
-    import sys
-
-
-    """request user information"""
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    """  request the entire TODO list for all users """
+    # Request the entire TODO list for all users
     name = user.json().get('username')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    """ CSV File Handling """
-    filename = userId + '.csv'
+    # CSV File Handling
+    filename = f"{user_id}.csv"
     with open(filename, mode='w') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL, lineterminator='\n')
         for task in todos.json():
-            if task.get('userId') == int(userId):
-                writer.writerow([userId, name, str(task.get('completed')),
+            if task.get('userId') == int(user_id):
+                writer.writerow([user_id, name, str(task.get('completed')),
                                  task.get('title')])
