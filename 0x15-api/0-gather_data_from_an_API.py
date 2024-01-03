26c0be91a966ed756505a891f8608a478ff4fc74
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-01-03 05:03:54 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,17 +9,19 @@ if __name__ == "__main__":
     restapi_url = "https://jsonplaceholder.typicode.com/"
 
     # GET request to retrieve user info
-    user_info = requests.get(restapi_url + "users/{}".format(sys.argv[1])).json()
+    user_info = requests.get(f"{restapi_url}users/{sys.argv[1]}").json()
 
     # GET request to retrieve the TODO list
-    todos = requests.get(restapi_url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(
+        f"{restapi_url}todos",
+        params={"userId": sys.argv[1]}
+    ).json()
 
     # Filter completed TODO list and store titles in a list
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [t.get("title") for t in todos if t.get("completed")]
 
-    # Print employee's name, completed tasks & total no of tasks. 
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_info.get("name"), len(completed), len(todos)))
+    # Print employee's name, completed tasks & total no of tasks.
+    print(f"Employee {user_info.get('name')} is done with tasks({len(completed)}/{len(todos)}):")
 
-    # Print the titles of completed tasks 
-    [print("\t {}".format(c)) for c in completed]
+    # Print the titles of completed tasks
+    [print(f"\t{c}") for c in completed]
