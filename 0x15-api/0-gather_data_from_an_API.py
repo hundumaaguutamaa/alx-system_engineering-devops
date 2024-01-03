7c29b1833bfac8663699fb6172f93c8b1358f79d
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-01-02 17:10:15 -0800

    Gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Gather data from an API"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    """Define REST API url"""
+    restapi_url = "https://jsonplaceholder.typicode.com"
+
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    # Get employee ID from command-line arguments
+    employee_id = int(sys.argv[1])
+
+    # Get employee details
+    user_response = requests.get(f"{restapi_url}/users/{employee_id}")
+    user_data = user_response.json()
+
+    # Get employee's TODO list
+    todo_response = requests.get(f"{restapi_url}/todos?userId={employee_id}")
+    todo_data = todo_response.json()
+
+    # Calculate progress
+    total_task = len(todo_data)
+    completed_task = sum(task['completed'] for task in todo_data)
+
+    # Display progress information
+    print(f"Employee {user_data['name']} is done with tasks ({completed_task}/{total_task}):")
+    print(f"{user_data['name']}:{completed_task}/{total_task}")
+
+
+
---
+++ 0x15-api/README.md
@@ -0,0 +1,6 @@
+	API
+What is an API
+What is a REST API
+What are microservices
+What is the CSV format
+What is the JSON format
