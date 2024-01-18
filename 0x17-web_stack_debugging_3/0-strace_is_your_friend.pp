6dc6a45dcec3c9ae812d2b4228a8ba48d051b182
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-01-18 15:25:04 -0800

    Fixes apache 500 error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Search for the string "phpp" and replace it with "php"
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
